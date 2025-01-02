5416b61c9551b0cf4c20e22451e77fe902744636
Ben Koch <178689076+bkoch-16@users.noreply.github.com>
2025-01-02 12:45:34 -0800

    resyncing with main
    resync with main

--- board.test.js
+++ board.test.js
@@ -1,18 +1,21 @@
-const Gameboard = require("./board")
-const Ship = require("./ship")
+const Gameboard = require("./board");
+const Ship = require("./ship");
 
-const board = new Gameboard()
-const ship = new Ship(5, 0, false)
-
+const board = new Gameboard();
+const ship = new Ship(5, 0, false);
 
 test("Test failed ship placement on board missing values", () => {
-    expect(board.placeShip(ship, ["C3", "C4"])).toBe(false);
+  expect(board.placeShip(ship, ["C3", "C4"])).toBe(false);
 });
 
 test("Test failed ship placement on board out of bounds", () => {
-    expect(board.placeShip(ship, ["A0", "A1", "A2", "A3", "A4"])).toBe(false);
+  expect(board.placeShip(ship, ["A0", "A1", "A2", "A3", "A4"])).toBe(false);
 });
 
 test("Test succeeded ship placement on board", () => {
-    expect(board.placeShip(ship, ["C3", "C4", "C5", "C6", "C7"])).toBe(true);
+  expect(board.placeShip(ship, ["C3", "C4", "C5", "C6", "C7"])).toBe(true);
+});
+
+test("Test shot that hits a ship", () => {
+  expect(board.receiveAttack("C4")).toBe(ship.hit());
 });
