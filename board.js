004e1b639ff8b13faa739698cc1ca76b336abed2
Ben Koch <178689076+bkoch-16@users.noreply.github.com>
2025-01-02 12:13:52 -0800

    first iteration of TDD with board
    created first iteration of TDD with board class

--- board.js
+++ board.js
@@ -1,12 +1,15 @@
 class Gameboard {
     constructor() {
         this.ships = []
+        this.board = {}
     }
 
-    placeShip(newShip) {
-        this.ships.push(newShip)
-    }
+    placeShip(newShip, coord) {
+        if (coord.length !== newShip.length) {
+            return false
+        }
+        else return true
 
+    }
 }
-
 module.exports = Gameboard
--- board.test.js
+++ board.test.js
@@ -4,7 +4,15 @@ const Ship = require("./ship")
 const board = new Gameboard()
 const ship = new Ship(5, 0, false)
 
-board.placeShip(ship)
-test("Test ship placement on board", () => {
-    expect(board.ships.length).toBeGreaterThan(0);
+
+test("Test failed ship placement on board missing values", () => {
+    expect(board.placeShip(ship, ["C3", "C4"])).toBe(false);
+});
+
+test("Test failed ship placement on board out of bounds", () => {
+    expect(board.placeShip(ship, ["A0", "A1", "A2", "A3", "A4"])).toBe(false);
+});
+
+test("Test succeeded ship placement on board", () => {
+    expect(board.placeShip(ship, ["C3", "C4", "C5", "C6", "C7"])).toBe(true);
 });
