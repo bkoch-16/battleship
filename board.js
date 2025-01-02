d5d29234bb037309cd50df3d6c5a5a1159fe1d1e
Ben Koch <178689076+bkoch-16@users.noreply.github.com>
2025-01-02 12:41:31 -0800

    setup out of bounds check for ship placement
    followed TDD for implementing out of bounds check for ship placement on board

--- board.js
+++ board.js
@@ -1,15 +1,23 @@
 class Gameboard {
-    constructor() {
-        this.ships = []
-        this.board = {}
-    }
+  constructor() {
+    this.ships = [];
+    this.board = {};
+  }
 
-    placeShip(newShip, coord) {
-        if (coord.length !== newShip.length) {
-            return false
+  placeShip(newShip, coord) {
+    if (coord.length !== newShip.length) {
+      return false;
+    } else {
+      for (const element of coord) {
+        const letter = element.slice(0, 1).toLowerCase();
+        const charCode = letter.charCodeAt(0);
+        const number = element.slice(1, 2);
+        if (charCode < 97 || charCode > 106 || number < 1 || number > 10) {
+          return false;
         }
-        else return true
-
+      }
+      return true;
     }
+  }
 }
-module.exports = Gameboard
+module.exports = Gameboard;
